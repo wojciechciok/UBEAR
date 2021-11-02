9aaa3a24c7ebba3e07d50d3c8831afa1420c2777
Wojciech Ciok <33331400+wojciechciok@users.noreply.github.com>
2021-11-02 10:57:05 +0100

    fixed variable name

--- frontend/src/sketch.js
+++ frontend/src/sketch.js
@@ -7,8 +7,8 @@ let inputMaxUpdates;
 let paragraphPassSpawnMin;
 let inputPassSpawnMin;
 //Passanger spawn maximum
-let paragraphPassSpawnMin;
-let inputPassSpawnMin;
+let paragraphPassSpawnMax;
+let inputPassSpawnMax;
 
 // size of the map (width and height)
 const size = 700;
@@ -75,16 +75,19 @@ function setup() {
 
   //Minimum interval time for passanger spawning
 
-  paragraphPassSpawnMin = createP("Minimum value for passenger spawning interval")
+  paragraphPassSpawnMin = createP(
+    "Minimum value for passenger spawning interval"
+  );
   paragraphPassSpawnMin.position(800, 30);
   inputPassSpawnMin = createInput("");
   inputPassSpawnMin.position(1120, 44);
   inputPassSpawnMin.style("width", "80px");
- 
 
   //Maximum interval time for passanger spawning
 
-  paragraphPassSpawnMax = createP("Maximum value for passenger spawning interval")
+  paragraphPassSpawnMax = createP(
+    "Maximum value for passenger spawning interval"
+  );
   paragraphPassSpawnMax.position(800, 60);
   inputPassSpawnMax = createInput("");
   inputPassSpawnMax.position(1120, 74);
@@ -240,11 +243,10 @@ function startSimulation() {
       maxUpdates: int(inputMaxUpdates.value()),
       no_visualization: noVisualizationCheckBox.checked(),
       minPassSpawn: int(inputPassSpawnMin.value()),
-      maxPassSpawn: int(inputPassSpawnMax.value())
-      
+      maxPassSpawn: int(inputPassSpawnMax.value()),
     },
     function (result) {
-      if (!noVisualizationCheckBox.checked()){
+      if (!noVisualizationCheckBox.checked()) {
         let guid = result.guid;
         // if successful allow animation
         const evtSource = new EventSource(`${url}/cars/positions/${guid}`);
