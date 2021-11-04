b4b319a70e3a357c36638b8c30d1c1bbd5b89ce4
Marcus <marcus2860@gmail.com>
2021-11-04 20:05:14 +0100

    Taxi Spawner implemented(needs testing)
    I have implemented the taxi spawning based on input from input field by user

--- frontend/src/sketch.js
+++ frontend/src/sketch.js
@@ -17,7 +17,7 @@ const cellNum = 21;
 // how wide is one block (how many cells separate two streets)
 const density = 5;
 // number of cars on the map
-const carsNumber = 15;
+let carsNumber= 0;
 // refresh rate
 const refreshRate = 15;
 
@@ -46,6 +46,10 @@ let button;
 
 // Road Construction, Taxi radio
 let radioBtn;
+
+// taxi spawner generator
+let taxiSpawnerTxt;
+let taxiSpawnerInp;
 
 // if checked, simulation will be performed without visualization (as fast as possible)
 let noVisualizationCheckBox;
@@ -113,6 +117,14 @@ function setup() {
 
   noVisualizationCheckBox = createCheckbox("No visualization", false);
   noVisualizationCheckBox.position(0, radioBtn.position()["y"] + 40);
+
+  // Create Taxi Spawner Generator
+  taxiSpawnerTxt = createP("Spawn taxis randomly");
+  taxiSpawnerTxt.position(radioBtn.position()["x"] + 256, radioBtn.position()["y"]);
+
+  taxiSpawnerInp = createInput("0");
+
+  taxiSpawnerInp.position(taxiSpawnerTxt.position()["x"] + 150, taxiSpawnerTxt.position()["y"]);
 }
 
 function update(data) {
@@ -198,6 +210,14 @@ function placeTaxi(id, x, y) {
   return new Car(id, x, y);
 }
 
+// spawns taxis based on the spawn taxi input field
+function spawnAmountOfTaxis(amount){
+  for (let i = 0; i < amount; i++) {
+    cars[carsIDs.length] = new Car(carsIDs.length);
+    carsIDs.push(carsIDs.length);
+  }
+}
+
 function startSimulation() {
   simulationStarted = true;
   for (let x = 0; x < map.grid.length; x++) {
@@ -208,6 +228,9 @@ function startSimulation() {
     }
   }
 
+  // Spawns taxis
+  carsNumber = int(taxiSpawnerInp.value());
+  spawnAmountOfTaxis(carsNumber);
   carsIDs = [];
   tmpCars = [];
   for (c in cars) {
@@ -222,14 +245,6 @@ function startSimulation() {
       invertedMap[invertedMap.length - 1].push(abs(col - 1));
     }
   }
-
-  // create cars
-  // const tmpCars = [];
-  // for (let i = 0; i < carsNumber; i++) {
-  //   cars[i] = new Car(i);
-  //   carsIDs.push(i);
-  //   tmpCars.push(cars[i]);
-  // }
 
   // send the map to the backend
   httpPost(
